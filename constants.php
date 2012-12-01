b1359d5f3635a77677c3211a3808149043ca651e
Diane Lee <dmclee@chikka.com>
2012-12-01 12:37:15 +0800

    changed type

--- constants.php
+++ constants.php
@@ -1,1 +1,14 @@
-/Applications/XAMPP/htdocs/sandbox_hackathon/constants.php
+<?php
+define('CLIENT_ID', 'DJYW5M54EASFQKEJ0NJXZUWIXOQXOXCEYZNRZ2Y4C352HBEU');
+define('CLIENT_SECRET', 'WMTH2TZ4IMDDTLOYJI22KLUCBPIPWHJFGN02JXMH54B1GAOW');
+define('REDIS_SERVER', '192.168.1.107');
+define('REDIS_PORT', '6379');
+
+##app:<venue_id> fields##
+define('BODY', 'body');
+define('PIC_URL', 'pic_url'); 
+define('START', 'start_time');
+define('END', 'end_time');
+define('LONGITUDE', 'long');
+define('LATITUDE', 'lat');
+?>
--- redis_utils.php
+++ redis_utils.php
@@ -1,1 +1,26 @@
-/Applications/XAMPP/htdocs/sandbox_hackathon/redis_utils.php
+<?php
+	
+	include('constants.php'); 
+
+	function connect_to_redis(){
+		$redis = new Redis(); 
+		$redis->connect(REDIS_SERVER, REDIS_PORT); 
+		return $redis;
+	}
+
+	function close_redis_connection($redis){
+		$redis->close();
+	}
+
+	function get_all_venue_details($redis, $venue_id){
+		$details = $redis->hGetAll($venue_id);
+		return $details;
+	}
+
+	function get_venue_detail($redis, $venue_id, $field_name){ 
+		$detail = $redis->hGet($venue_id, $field_name);
+		return $detail;
+	}
+
+
+?>
